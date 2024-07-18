f18496eebef3b6bfdff4c8f7e2b48b9af8114ff4
ThanyaphornSaetao347 <tunyaporn4875@gmail.com>
2024-07-18 17:37:50 +0700

    first commit

---
+++ Assets/Scripts/TurretControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretControl : MonoBehaviour
+{
+    // This script allows the user to rotate the turret,
+    // and raise and lower the barrel elevation.
+    public Transform barrel;
+    public float min = -30;
+    public float max = 30;
+
+    float elevation = 0;
+
+    void Update()
+    {
+        // horizontal rotation control
+        // transform.Rotate(x,y,z);
+        transform.Rotate(0, Input.GetAxis("Horizontal"), 0); // Horizontal แนวนอน
+
+        // barrel elevation control
+        float v = Input.GetAxis("Vertical"); // Vertical แนวตั้ง
+        elevation = Mathf.Clamp(elevation+v,min,max);
+        barrel.localRotation = Quaternion.Euler(elevation,0,0);
+    }
+}
